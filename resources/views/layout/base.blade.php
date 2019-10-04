454ff33906934e6ad76d4bb97c9c230675474243
Edoardo <a@Air-di-edo.lan>
2019-10-04 11:03:51 +0200

    aggiunto css per postsindex

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -8,12 +8,12 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="{{mix('css/app.css')}}">
         </style>
     </head>
     <body>
       @include('elem.header')
-      
+
       @yield('content')
 
       @include('elem.footer')
--- resources/views/pages/postsindex.blade.php
+++ resources/views/pages/postsindex.blade.php
@@ -1,16 +1,22 @@
 @extends('layout.base')
 
 @section('content')
-  <a href="{{route('posts.create')}}">NUOVO POST</a>
-  <h1>I Post</h1>
-  @foreach ($posts as $key => $value)
-    <div class="the-post">
-      <h2>{{$value-> title}}</h2>
-      <h3>{{$value->author}}</h3>
-      <p>{{$value->description}}</p>
-    </div>
-    <p> <a href="{{route('posts.edit', $value->id)}}">Modifica Post...</a></p>
-    <p> <a href="{{route('posts.destroy', $value->id)}}">!!ELIMINA POST!!</a></p>
-    <hr>
-  @endforeach
+  <div class="post-action new-post">
+    <a href="{{route('posts.create')}}">NUOVO POST</a>
+  </div>
+
+  <div class="container">
+    @foreach ($posts as $key => $value)
+      <div class="the-post">
+        <h2>{{$value-> title}}</h2>
+        <h3>{{$value->author}}</h3>
+        <p>{{$value->description}}</p>
+
+        <div class="post-action edit-post"> <a href="{{route('posts.edit', $value->id)}}">Modifica Post...</a></div>
+        <div class="post-action delete-post"> <a href="{{route('posts.destroy', $value->id)}}">!!ELIMINA POST!!</a></div>
+      </div>
+
+    @endforeach
+  </div>
+
 @endsection
